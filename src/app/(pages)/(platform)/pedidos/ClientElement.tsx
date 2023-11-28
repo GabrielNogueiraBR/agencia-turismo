c812058f81339312eca736d8d2fa2ba792745fd6
Gabriel Nogueira <gabrielnoog7@gmail.com>
2023-11-27 22:53:16 -0300

    feat: update

---
+++ src/app/(pages)/(platform)/pedidos/ClientElement.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+import React from "react";
+
+const ClientElement = () => {
+  return <div>ClientElement</div>;
+};
+
+export default ClientElement;
--- src/components/Header/UserInfo.tsx
+++ src/components/Header/UserInfo.tsx
@@ -1,12 +1,15 @@
-import { Avatar, Flex, Link, Text, VStack } from "@chakra-ui/react";
+"use client";
+
 import React from "react";
+import { Avatar, Flex, Link, Text, VStack } from "@chakra-ui/react";
+import NextLink from "next/link";
 
 const UserInfo = () => {
   return (
     <Flex direction="row" justify="center" align="center" gap="4">
       <VStack spacing={0} align="flex-end">
-        <Text fontSize={'xl'}>Gabriel Nogueira</Text>
-        <Link href="/pedidos" color="blueviolet">
+        <Text fontSize={"xl"}>Gabriel Nogueira</Text>
+        <Link as={NextLink} href="/pedidos" color="blueviolet">
           meus pedidos
         </Link>
       </VStack>
