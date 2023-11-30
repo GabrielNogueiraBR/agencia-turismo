d08c0e3ea2d57e985d766a37a2c9472bf367e38f
Gabriel Nogueira <gabrielnoog7@gmail.com>
2023-11-30 13:02:10 -0300

    feat: adding types

---
+++ src/types/hotelApi.ts
@@ -0,0 +1,16 @@
+export type UserCreateDto = {
+  name: string;
+  email: string;
+  taxId: string;
+  phone: string;
+  birthdate: string;
+};
+
+export type User = {
+  id: string;
+  name: string;
+  email: string;
+  taxId: string;
+  phone: string;
+  birthdate: string;
+};
