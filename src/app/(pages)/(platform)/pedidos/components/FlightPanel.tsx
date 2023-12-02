60a892f8678d69999eeba1f47a01abdff7a090e8
Gabriel Nogueira <gabrielnoog7@gmail.com>
2023-12-01 22:02:38 -0300

    feat: flight

--- src/app/(pages)/(platform)/pedidos/components/FlightPanel.tsx
+++ src/app/(pages)/(platform)/pedidos/components/FlightPanel.tsx
@@ -1,8 +1,10 @@
 'use client'
 
-import FlightSeats from '@/components/FlightSeats'
+import CustomDataTable from '@/components/CustomDataTable'
+import LoadingPage from '@/components/LoadingPage'
+import { useAuth } from '@/context/AuthContext'
 import flightApi from '@/service/flightApi'
-import { FlightPresenter } from '@/types/flightApi'
+import { ReservationPresenter } from '@/types/flightApi'
 import { TabPanel, TabPanelProps } from '@chakra-ui/react'
 import { useQuery } from '@tanstack/react-query'
 import React from 'react'
@@ -10,18 +12,88 @@ import React from 'react'
 interface Props extends TabPanelProps {}
 
 const FlightPanel = ({ ...rest }: Props) => {
-  const {
-    isPending,
-    error,
-    data: flights,
-  } = useQuery<FlightPresenter[]>({
-    queryKey: ['flightsList'],
-    queryFn: () => flightApi.get('/flights').then((res) => res.data.data),
+  const { token } = useAuth()
+
+  const { isLoading, data: bookings } = useQuery<ReservationPresenter[]>({
+    queryKey: ['reservationsList'],
+    queryFn: () =>
+      flightApi
+        .get('/users/me/reservations', { headers: { Authorization: `Bearer ${token}` } })
+        .then((res) => res.data),
   })
 
   return (
-    <TabPanel display="flex" flexDirection="column" pt="8" p="6" gap="4" w="100%" {...rest}>
-      {flights?.map((flight) => <FlightSeats key={flight.id} flight={flight} />)}
+    <TabPanel
+      display="flex"
+      flexDirection="column"
+      p="0"
+      gap="4"
+      w="100%"
+      overflow="hidden"
+      {...rest}
+    >
+      {!bookings && <LoadingPage />}
+      {bookings && (
+        <CustomDataTable
+          defaultSortFieldId="date"
+          defaultSortAsc={false}
+          columns={[
+            {
+              name: 'Cidade origem',
+              center: true,
+              sortable: true,
+              selector: (row) => row.flight.departureAirport.city,
+              wrap: true,
+            },
+            {
+              name: 'Cidade destino',
+              center: true,
+              sortable: true,
+              selector: (row) => row.flight.arrivalAirport.city,
+              wrap: true,
+            },
+            {
+              id: 'departure',
+              name: 'Partida',
+              center: true,
+              selector: (row) => row.flight.departureTime,
+              sortable: true,
+              wrap: true,
+              format: (row) =>
+                new Intl.DateTimeFormat('pt-BR', {
+                  timeZone: 'UTC',
+                  month: 'long',
+                  year: 'numeric',
+                  day: '2-digit',
+                }).format(new Date(row.flight.departureTime)),
+            },
+            {
+              id: 'arrival',
+              name: 'Chegada',
+              center: true,
+              selector: (row) => row.flight.arrivalTime,
+              sortable: true,
+              wrap: true,
+              format: (row) =>
+                new Intl.DateTimeFormat('pt-BR', {
+                  timeZone: 'UTC',
+                  month: 'long',
+                  year: 'numeric',
+                  day: '2-digit',
+                }).format(new Date(row.flight.arrivalTime)),
+            },
+            {
+              name: 'Assento',
+              center: true,
+              selector: (row) => row.seatNumber,
+              sortable: true,
+              wrap: true,
+            },
+          ]}
+          data={bookings}
+          progressPending={isLoading}
+        />
+      )}
     </TabPanel>
   )
 }
