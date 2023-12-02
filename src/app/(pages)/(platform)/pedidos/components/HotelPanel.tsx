f67768877436130d803db31c5cb1eae6c44354b9
Gabriel Nogueira <gabrielnoog7@gmail.com>
2023-12-01 22:30:44 -0300

    feat: adding cancel booking method

--- src/app/(pages)/(platform)/pedidos/components/HotelPanel.tsx
+++ src/app/(pages)/(platform)/pedidos/components/HotelPanel.tsx
@@ -1,20 +1,59 @@
 'use client'
 
+import React, { useState } from 'react'
+
 import CustomDataTable from '@/components/CustomDataTable'
 import LoadingPage from '@/components/LoadingPage'
 import hotelApi from '@/service/hotelApi'
 import { BookingDto } from '@/types/hotelApi'
-import { TabPanel, TabPanelProps } from '@chakra-ui/react'
+import { Button, Icon, TabPanel, TabPanelProps, Tooltip, useToast } from '@chakra-ui/react'
 import { useQuery } from '@tanstack/react-query'
-import React from 'react'
+import { BiSolidXCircle } from 'react-icons/bi'
+import { GoTrash } from 'react-icons/go'
 
 interface Props extends TabPanelProps {}
 
 const HotelPanel = ({ ...rest }: Props) => {
-  const { isLoading, data: bookings } = useQuery<BookingDto[]>({
+  const [isCancelling, setIsCancelling] = useState<boolean>(false)
+
+  const toast = useToast()
+
+  const {
+    isLoading,
+    data: bookings,
+    refetch,
+  } = useQuery<BookingDto[]>({
     queryKey: ['bookingsList'],
     queryFn: () => hotelApi.get('/bookings').then((res) => res.data.records),
   })
+
+  const cancelBooking = async (bookingId: string) => {
+    try {
+      if (isCancelling) return
+      setIsCancelling(true)
+
+      await hotelApi.delete(`/bookings/${bookingId}`)
+
+      toast({
+        title: 'Reserva cancelada com sucesso!',
+        status: 'success',
+        duration: 5000,
+        isClosable: true,
+      })
+      await refetch()
+    } catch (e) {
+      console.error(e)
+      toast({
+        title: 'Erro no cancelamento da reserva',
+        description: e instanceof Error ? e.message : 'Ops, tente novamente.',
+        status: 'error',
+        duration: 5000,
+        isClosable: true,
+      })
+    } finally {
+      setIsCancelling(false)
+    }
+  }
 
   return (
     <TabPanel
@@ -32,12 +71,6 @@ const HotelPanel = ({ ...rest }: Props) => {
           defaultSortFieldId="date"
           defaultSortAsc={false}
           columns={[
-            {
-              name: 'ID',
-              center: true,
-              selector: (row) => row.id,
-              wrap: true,
-            },
             {
               id: 'checkIn',
               name: 'Check-in',
@@ -72,6 +105,33 @@ const HotelPanel = ({ ...rest }: Props) => {
               selector: (row) => row.status,
               sortable: true,
               wrap: true,
+            },
+            {
+              name: 'Ação',
+              center: true,
+              selector: (row) => row.id,
+              sortable: true,
+              wrap: true,
+              cell: (row) => {
+                return (
+                  <Tooltip label="Cancelar reserva" placement="top" hasArrow>
+                    <Button
+                      colorScheme="none"
+                      p={0}
+                      m={0}
+                      w="fit-content"
+                      h="fit-content"
+                      minW={0}
+                      minH={0}
+                      aspectRatio={1}
+                      onClick={() => cancelBooking(row.id)}
+                      isLoading={isCancelling}
+                    >
+                      <Icon as={GoTrash} bg="transparent" color="red" rounded="md" fontSize="2xl" />
+                    </Button>
+                  </Tooltip>
+                )
+              },
             },
           ]}
           data={bookings}
