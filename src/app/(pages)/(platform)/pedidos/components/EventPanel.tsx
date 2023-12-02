2557b3667c8ec498cee826b07b7b47c7bed83aa8
Gabriel Nogueira <gabrielnoog7@gmail.com>
2023-12-02 10:42:43 -0300

    feat: update event panel

--- src/app/(pages)/(platform)/pedidos/components/EventPanel.tsx
+++ src/app/(pages)/(platform)/pedidos/components/EventPanel.tsx
@@ -1,26 +1,30 @@
 'use client'
 
-import eventApi from '@/service/eventApi'
-import { Event } from '@/types/eventApi'
-import { TabPanel, TabPanelProps } from '@chakra-ui/react'
-import { useQuery } from '@tanstack/react-query'
 import React from 'react'
+
+import CustomDataTable from '@/components/CustomDataTable'
+import { TabPanel, TabPanelProps } from '@chakra-ui/react'
 
 interface Props extends TabPanelProps {}
 
 const EventPanel = ({ ...rest }: Props) => {
-  const {
-    isPending,
-    error,
-    data: events,
-  } = useQuery<Event[]>({
-    queryKey: ['eventsList'],
-    queryFn: () => eventApi.get('/events').then((res) => res.data.content),
-  })
-
   return (
-    <TabPanel display="flex" flexDirection="column" pt="8" p="6" gap="4" w="100%" {...rest}>
-      {events?.map((event) => event.name)}
+    <TabPanel
+      display="flex"
+      flexDirection="column"
+      p="0"
+      gap="4"
+      w="100%"
+      overflow="hidden"
+      {...rest}
+    >
+      <CustomDataTable
+        defaultSortFieldId="date"
+        defaultSortAsc={false}
+        columns={[]}
+        data={[]}
+        progressPending={false}
+      />
     </TabPanel>
   )
 }
